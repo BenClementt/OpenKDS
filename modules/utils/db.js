c9840c4fb1bda1f9a3b1c8221b2c2a6c584e5e15
Ben Clement <ben.caulfield@outlook.com>
2023-03-16 13:01:57 +0000

    fix: add port to mysql setup

--- modules/utils/db.js
+++ modules/utils/db.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
